0136d896109e5cf3ddc08d131a7ecf84d8632bbb
firstplayer34 <pas.vorobiov@gmail.com>
2020-02-02 17:32:25 +0300

    Worked with classes

--- main.py
+++ main.py
@@ -2,36 +2,31 @@ from constants import TOKEN
 import requests
 import vk_api
 from vk_api.longpoll import VkLongPoll, VkEventType
-import random
-import re
-from datetime import datetime
-from patterns import *
+from patterns import patterns
 
-
-vk_session = vk_api.VkApi(token=TOKEN)
-longpoll = VkLongPoll(vk_session)
-vk = vk_session.get_api()
-for event in longpoll.listen():
-    if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
-        if re.match(pattern_hello, event.text.lower()) is not None:
+#Working with patterns and returning a more comfortable answer
+def find_pattern(text):
+    s = ""
+    used_responses = []
+    import re
+    for i in patterns:
+        if re.match(i[0], text.lower(), re.IGNORECASE) is not None:
+            print(i)
+            for j in i[1]:
+                s+=j+'\n'
+            used_responses.append(i)
+    return s, used_responses
+if __name__ == "__main__":
+    find_pattern("привет")
+    vk_session = vk_api.VkApi(token=TOKEN)
+    longpoll = VkLongPoll(vk_session)
+    vk = vk_session.get_api()
+    for event in longpoll.listen():
+        if event.type == VkEventType.MESSAGE_NEW and event.to_me and event.text:
+            message_text = find_pattern(event.text)[0]
+            import random
             vk.messages.send(
                 user_id = event.user_id,
-                message = hello_message,
-                random_id = random.randint(0,1000000)
+                message = message_text,
+                random_id = random.randint(0,10000000)
             )
-            vk.messages.send(
-                user_id = event.user_id,
-                message = review_message,
-                random_id = random.randint(0,1000000)
-            )
-            with open("output.txt", "a") as o:
-                o.write("hello_request at {} from {}\n".format(datetime.now(),event.user_id))
-        if re.match(pattern_time, event.text.lower()) is not None:
-            vk.messages.send(
-                user_id = event.user_id,
-                message = "Московское время: "+str(datetime.now()),
-                random_id = random.randint(0,1000000)
-            )
-            with open("output.txt", "a") as o:
-                o.write("time_request at {} from {}\n".format(datetime.now(),event.user_id))
-            
